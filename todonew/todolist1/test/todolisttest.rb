a3eaa536d76dd786d3f28da750a24125c55e7e9a
surekapriya <surekapriya@railsfactory.org>
2014-07-11 18:20:55 +0530

    new todo task

--- todonew/todolist1/test/todolisttest.rb
+++ todonew/todolist1/test/todolisttest.rb
@@ -1,18 +1,16 @@
-require_relative '../lib/todolist.rb'
+require_relative '../lib/todolist'
 require 'test/unit'
 class Todolisttest < Test::Unit::TestCase
 
 def setup
 
- @t = Todolist.new("aaa.txt")
+ @t = Todolist.new("abc.txt")
 
 end
 
 def teardown
  @t=nil
 end
-
-
 
 
  def test_aempty
@@ -109,13 +107,84 @@ def test_modify
  end
 
 def test_pending
-    @t.show_pending(1)
+# precondition
+@t.empty
+@t.add("one")
+@t.add("two")
+
+#before state
+assert_equal 2,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 2,@t.list.size
+
+#action
+@t.show_pending(1)
+
+#after
+assert_equal 2,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 2,@t.list.size
+assert_equal "one",@t.show_pending(1)
+   
 end
 def test_qcompleted
-    @t.show_completed(0)
+# precondition
+@t.empty
+@t.add("one")
+@t.add("two")
+@t.add("three")
+
+#before state
+assert_equal 3,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 3,@t.list.size
+
+#action
+@t.complete(1)
+ 
+#after
+assert_equal 2,@t.pending.size
+assert_equal 1,@t.completed.size
+assert_equal 3,@t.list.size
+assert_equal "one",@t.show_completed(1)
+                 
+   
 end
+def test_storage
+# precondition
+@t.empty
+@t.add("five")
+@t.add("six")
+@t.add("seven")
 
-
+#before state
+assert_equal 3,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 3,@t.list.size
+                 
+#action
+@t.complete(1)
+@t.save
+#after saving
+assert_equal 2,@t.pending.size
+assert_equal 1,@t.completed.size
+assert_equal 3,@t.list.size
+assert_equal "five",@t.show_completed(1)
+                 
+ #deleting the elements of array
+@t.empty
+assert_equal 0,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 0,@t.list.size
+                 
+  # calling load function
+ @t.load1
+  #after loading
+assert_equal 2,@t.pending.size
+assert_equal 1,@t.completed.size
+assert_equal 3,@t.list.size
+end
+               
 end
 
 
